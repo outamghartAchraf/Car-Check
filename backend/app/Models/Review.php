6a759d83076e397cb583b508861df54bde57487e
outamghartAchraf <outamghart2000@gmail.com>
2026-09-06 16:47:49 +0100

    add fillable and relationship

--- backend/app/Models/Review.php
+++ backend/app/Models/Review.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+       protected $fillable = [
+        'inspection_report_id',
+        'client_id',
+        'mechanic_id',
+        'rating',
+        'comment',
+    ];
+
+    protected $casts = [
+        'rating' => 'integer',
+    ];
+
+    public function inspectionReport()
+    {
+        return $this->belongsTo(InspectionReport::class);
+    }
+
+    public function client()
+    {
+        return $this->belongsTo(User::class, 'client_id');
+    }
+
+    public function mechanic()
+    {
+        return $this->belongsTo(User::class, 'mechanic_id');
+    }
 }
